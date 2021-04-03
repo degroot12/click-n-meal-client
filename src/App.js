4e0fdfe1cd7d5d00657f4d63940a791dcf073ee3
Cindy <cindytvn+git@gmail.com>
2021-04-03 14:17:08 +0200

    Simple errorpage prepared

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import { Switch, Route } from "react-router-dom";
 // import axios from "axios";
 // import config from "../config.js";
 import MyNav from './components/MyNav'
+import Home from './components/Home'
+import Error from './components/Error'
+
+// TODO: add to request with login { withCredentials: true }
+
 
 
 function App(props) {
+  const [error, setError] = useState(null);
 
   return(
     <div>
       <MyNav />
-      <h1>The homepage</h1>
       <Switch>
-        <Route exact path="/" render={() => {
-          return <Home />
+        <Route 
+          exact path="/" 
+          render={() => {
+            return <Home />
         }} />
+
+        <Route 
+          path='/signin'
+          render={() => {
+            return <Signin />
+          }} />
+
+        
 
 
       </Switch>
---
+++ src/components/Error.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Error () {
+    return (
+        <div>
+            <h1>Snapp! Something went wrong!</h1>
+            <Image 
+                scr="https://res.cloudinary.com/snowbird/image/upload/v1617451630/Click-n-meal/broken_plate_q0yme7.jpg" 
+                alt="broken plate"
+            />
+        </div>
+    )
+}
+
+export default Error
