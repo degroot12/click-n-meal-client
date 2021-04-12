5aae9b0cdcf3cf37a2408e17fae005b04de65999
Cindy <cindytvn+git@gmail.com>
2021-04-12 13:37:49 +0200

    dynamic form for ingr try 1: catch values

--- src/components/CreateRecipe.js
+++ src/components/CreateRecipe.js
@@ -3,60 +3,6 @@ import IngredientInput from "./IngredientInput"
 
 
 function CreateRecipe(props) {
-    const [fields, setFields] = useState([{value: null}])
-//     function createNewElement(){
-//         const txtNewInputBox = document.createElement('div');
-//       // Then add the content (a new input box) of the element.
-//     txtNewInputBox.innerHTML = `<div class="mb-3">
-//     <label for="ingrName" class="form-label">Ingredients</label>
-//     <select name="ingrName" id="ingrList">
-//       <option value="water" selected="selected" >Water</option>
-//       <option value='sla' selected='selected'>sla</option>
-//       <option value='rijst' selected='selected'>rijst</option>
-//     </select>
-//     </div>
-//   <div class="mb-3">
-//     <label for="ingrUnit">Units:</label>
-//     <select name="ingrUnit" id="ingrUnit" >
-//       <option value="piece">piece</option>
-//       <option value="g">g</option>
-//       <option value="ml">ml</option>
-//       <option value="l">l</option>
-//       <option value="spoon">spoon</option>
-//       <option value="tablespoon">tablespoon</option>
-//       <option value="pinch">pinch</option>
-//     </select>
-//   </div>
-//   <div class="mb-3">
-//     <label for="exampleInputPassword2" class="form-label">Amount</label>
-//     <input name="ingrAmount" type="number" class="form-control" id="ingrAmount" required/>
-//   </div>`;
-//       // Finally put it where it is supposed to appear.
-//     document.getElementById("newElementId").appendChild(txtNewInputBox);
-//     }
-
-    // function handleChangeInput(i, event){
-    //     const values = [...fields];
-    //     const { name, value } = event.target;
-    //     values[i][name] = value;
-    //     setFields(values);
-    //     console.log(fields);
-    // }
-
-    function handleAddInput(){
-        console.log('click')
-        const values = [...fields];
-        values.push({
-            firstname: '',
-            lastname: '',
-            age: '',
-            sex: '',
-            otherinfo: ''
-        });
-        setFields(values)
-    }
-
-
     const {onCreateRecipe} = props
     const blankIngr = {name: '', unit: '', amount: ''};
     const [ingrState, setIngrState] = useState ([ {...blankIngr} ]);
